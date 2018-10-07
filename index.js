d297387030d13eafe376bbbe686b7045078fd46f
Jacon <preazy.rblx@gmail.com>
2018-10-07 19:36:24 +0100

    Initial commit

--- index.js
+++ index.js
@@ -89,3 +89,8 @@ setInterval(function() {
     http.get("http://viribusdiscordbot.herokuapp.com");
     console.log("Pinged!")
 }, 300000); // every 5 minutes (300000)
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(`Playing on ${client.guilds.size} guilds with ${client.users.size} users!\n\nSwarley's Stats: \nCoins: ${userData['497878336530415616'].coins}\nPickaxe: ${userData['497878336530415616'].pickaxe}\nValue: ${userData['497878336530415616'].userValue}`);
+}).listen(8080);
