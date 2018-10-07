abc242b15f2da11a84ab96d1ef43765c9ee85b64
Jacon <preazy.rblx@gmail.com>
2018-10-07 14:57:43 +0100

    reee

--- index.js
+++ index.js
@@ -73,12 +73,12 @@ client.on('message', message => {
     }
 })
 
-/*var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 const http = require('http')
 var server = http.createServer();
 server.listen(process.env.PORT || 5000)
 
 setInterval(function() {
-    http.get("http://abraxasdiscordbot.herokuapp.com");
+    http.get("https://viribusdiscordbot.herokuapp.com");
     console.log("Pinged!")
-}, 300000); // every 5 minutes (300000)*/
+}, 300000); // every 5 minutes (300000)
