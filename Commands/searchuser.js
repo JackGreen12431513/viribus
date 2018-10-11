e548589919b09510f8bb58023c35d5328eb49a4f
JackGreen12431513 <42324915+JackGreen12431513@users.noreply.github.com>
2018-10-11 22:58:35 +0100

    Made it smexy

--- Commands/searchuser.js
+++ Commands/searchuser.js
@@ -8,7 +8,7 @@ exports.run = (client, message, args, ops, eEmb) => {
         if(!searchTerm) return message.channel.send("Please supply valid paramaters!")
         
         let matches = users.filter(u => u.name.toLowerCase().includes(searchTerm.toLowerCase()));
-        var matchF = matches.map(u => u.name).join("\n")
+        var matchF = matches.map(u => u.name + " - " + u.owner.tag).join("\n")
         if(matchF == "") {
             var emb = new Discord.RichEmbed()
             .setTitle("No Users Found!")
